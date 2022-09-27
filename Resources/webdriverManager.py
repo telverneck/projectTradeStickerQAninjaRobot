dabac85521d71ae76a5fe56c908de2748bef887e
telmoCorrea <telverneck@hotmail.com>
2022-09-27 14:51:30 -0400

    Add Edge to WebDriver Manager

--- Resources/webdriverManager.py
+++ Resources/webdriverManager.py
@@ -1,5 +1,6 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
 def get_driver_path_with_browser(browser_name):
@@ -7,5 +8,7 @@ def get_driver_path_with_browser(browser_name):
         driver_path = ChromeDriverManager().install()
     elif browser_name.lower() == 'firefox':
         driver_path = GeckoDriverManager().install()
+    else:
+        driver_path = EdgeChromiumDriverManager.install()
     print(driver_path)
     return driver_path
